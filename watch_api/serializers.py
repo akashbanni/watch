15666f8e8518b23f6eee78dc87618ce5af4c41cf
Akash <akashbanni.ab.1357@gmail.com>
2024-03-16 11:14:24 +0530

    commit

--- watch_api/serializers.py
+++ watch_api/serializers.py
@@ -10,23 +10,23 @@ class MovieModelSerializer(serializers.Serializer):
     release_year = serializers.IntegerField();
     rating = serializers.FloatField();
     
-    # def create(self, validated_data):
-    #     """
-    #     Create and return a new `watch_movie` instance, given the validated data.
-    #     """
-    #     return watch_movie.objects.create(**validated_data)
+    def create(self, validated_data):
+        """
+        Create and return a new `watch_movie` instance, given the validated data.
+        """
+        return watch_movie.objects.create(**validated_data)
     
-    # def update(self, instance, validated_data):
-    #     """
-    #     Update and return an existing `watch_movie` instance, given the validated data.
-    #     """
-    #     instance.movie_name = validated_data.get('movie_name', instance.movie_name)
-    #     instance.director_name = validated_data.get('director_name', instance.director_name)
-    #     instance.story_line = validated_data.get('story_line', instance.story_line)
-    #     instance.release_year = validated_data.get('release_year', instance.release_year)
-    #     instance.rating = validated_data.get('rating', instance.rating)
-    #     instance.save()
-    #     return instance
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `watch_movie` instance, given the validated data.
+        """
+        instance.movie_name = validated_data.get('movie_name', instance.movie_name)
+        instance.director_name = validated_data.get('director_name', instance.director_name)
+        instance.story_line = validated_data.get('story_line', instance.story_line)
+        instance.release_year = validated_data.get('release_year', instance.release_year)
+        instance.rating = validated_data.get('rating', instance.rating)
+        instance.save()
+        return instance
 
 class PlatformModelSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -35,18 +35,18 @@ class PlatformModelSerializer(serializers.Serializer):
     Platform_url = serializers.URLField()
 
 
-    # def create(self, validated_data):
-    #     """
-    #     Create and return a new `watch_movie` instance, given the validated data.
-    #     """
-    #     return watch_platform.objects.create(**validated_data)
+    def create(self, validated_data):
+        """
+        Create and return a new `watch_movie` instance, given the validated data.
+        """
+        return watch_platform.objects.create(**validated_data)
     
-    # def update(self, instance, validated_data):
-    #     """
-    #     Update and return an existing `watch_movie` instance, given the validated data.
-    #     """
-    #     instance.Platform = validated_data.get('Platform', instance.Platform)
-    #     instance.platform_name = validated_data.get('platform_name', instance.platform_name)
-    #     instance.Platform_url = validated_data.get('Platform_url', instance.Platform_url)
-    #     instance.save()
-    #     return instance
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `watch_movie` instance, given the validated data.
+        """
+        instance.Platform = validated_data.get('Platform', instance.Platform)
+        instance.platform_name = validated_data.get('platform_name', instance.platform_name)
+        instance.Platform_url = validated_data.get('Platform_url', instance.Platform_url)
+        instance.save()
+        return instance
--- watch_api/views.py
+++ watch_api/views.py
@@ -1,29 +1,107 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
 from .models import *
+from rest_framework.decorators import api_view
 from .serializers import *
+from rest_framework import status
 # Create your views here.
+
+@api_view(('GET','POST'))
 def watch_list(request):
+    if request.method == 'GET':
+        movie_list = watch_movie.objects.all()
+        serializers_movie=MovieModelSerializer(movie_list, many=True)
+        return Response(serializers_movie.data)
+    
+    elif request.method == 'POST':
+        serializer = MovieModelSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    movie_list = watch_movie.objects.all()
-    serializers_movie=MovieModelSerializer(movie_list, many=True)
-    return JsonResponse(serializers_movie.data, safe=False)
+# @api_view(('GET','POST'))
+# def watch_list_by_id(request,pk):
 
-def watch_list_by_id(request,pk):
+#     if request.method == 'GET':
+#         movie_list_by_id = watch_movie.objects.get(pk = pk)
+#         serializer_movie_by_id =MovieModelSerializer(movie_list_by_id)
+#         return Response(serializer_movie_by_id.data)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def watch_list_by_id(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = watch_movie.objects.get(pk=pk)
+    except watch_movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    movie_list_by_id = watch_movie.objects.get(pk = pk)
-    serializer_movie_by_id =MovieModelSerializer(movie_list_by_id)
+    if request.method == 'GET':
+        serializer = MovieModelSerializer(snippet)
+        return Response(serializer.data)
 
-    return JsonResponse(serializer_movie_by_id.data)
+    elif request.method == 'PUT':
+        serializer = MovieModelSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    # movie_list_by_id = watch_movie.objects.get(pk = pk)
+    # serializer_movie_by_id =MovieModelSerializer(movie_list_by_id)
+
+    # return Response(serializer_movie_by_id.data)
+
+@api_view(('GET','POST'))
 def platform_list(request):
+    if request.method=='GET':
+        platform_list_movie = watch_platform.objects.all()
+        serializers_platform = PlatformModelSerializer(platform_list_movie,many=True)
+        return Response(serializers_platform.data)
+    
+    elif request.method == 'POST':
+        serializers_platform = PlatformModelSerializer(data=request.data)
+        if serializers_platform.is_valid():
+            serializers_platform.save()
+            return Response(serializers_platform.data, status=status.HTTP_201_CREATED)
+        return Response(serializers_platform.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    platform_list_movie = watch_platform.objects.all()
-    serializers_platform = PlatformModelSerializer(platform_list_movie,many=True)
-    return JsonResponse(serializers_platform.data,safe=False)
 
-def platform_list_by_id(request,pk):
 
-    platform_list_by_id = watch_platform.objects.get(pk=pk)
-    serializers_platform_by_id = PlatformModelSerializer(platform_list_by_id)
-    return JsonResponse(serializers_platform_by_id.data)
+# @api_view(('GET','POST'))
+# def platform_list_by_id(request,pk):
+
+#     platform_list_by_id = watch_platform.objects.get(pk=pk)
+#     serializers_platform_by_id = PlatformModelSerializer(platform_list_by_id)
+#     return Response(serializers_platform_by_id.data)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def platform_list_by_id(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = watch_platform.objects.get(pk=pk)
+    except watch_platform.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PlatformModelSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = PlatformModelSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
